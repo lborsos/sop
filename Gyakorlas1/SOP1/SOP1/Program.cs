4b674a1864bf24f03ba99bc0e7befe39faf18d12
Borsos Laszlo <lborsos@gmail.com>
2025-01-07 02:17:59 +0200

    First commit

---
+++ Gyakorlas1/SOP1/SOP1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SOP1
+{
+    internal class Program
+    {
+        const int N = 1000000;
+        static int[] data = new int[N];
+        static int sum = 0;
+        static void proc1()
+        {
+            for (int i = 0; i < N/2; i++)
+            {
+                // lock (typeof(Program) {; }
+                lock (data)
+                {
+                    sum += data[i];
+                }
+                // Console.WriteLine(i);
+            }
+        }
+        static void proc2()
+        {
+            for (int i = N/2; i < N; i++)
+            {
+                lock (data)
+                {
+                    sum += data[i];
+                }
+                // Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < N; i++) {
+                data[i] = i;
+            }
+            Thread t1 = new Thread(proc1); t1.Priority = ThreadPriority.Highest;
+            Thread t2 = new Thread(proc2); t2.Priority = ThreadPriority.Normal;
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+            Console.WriteLine(sum);
+            //proc1();
+            //proc2();
+        }
+    }
+}
---
+++ Gyakorlas1/SOP1/SOP1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SOP1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SOP1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a5ff7d43-bdee-4310-b5ed-2591ef3952ef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
